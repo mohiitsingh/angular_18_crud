a49918027a446596bb182f3adbc2545dded26c01
mohiitsingh <ms6065097@gmail.com>
2024-12-31 01:02:55 +0530

    Employee component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeModel } from './model/Employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_18';
+  employeeForm : FormGroup = new FormGroup({});
+
+  employeeObj: EmployeeModel = new EmployeeModel();
+  employeeList: EmployeeModel [] = [];
+
+  constructor(){
+    debugger;
+    this.createForm();
+    const oldData = localStorage.getItem("EmpData");
+    if(oldData != null){
+      const parseData = JSON.parse(oldData);
+      this.employeeList = parseData;
+    }
+  }
+
+  createForm(){
+    this.employeeForm = new FormGroup({
+      empid: new FormGroup(this.employeeObj.empId),
+      name: new FormGroup(this.employeeObj.name),
+      city: new FormGroup(this.employeeObj.city),
+      address: new FormGroup(this.employeeObj.address),
+      pincode: new FormGroup(this.employeeObj.pincode),
+      contactNumber: new FormGroup(this.employeeObj.contactNumber),
+      emailId: new FormGroup(this.employeeObj.emailId),
+      state: new FormGroup(this.employeeObj.state),
+
+    })
+  }
+
+  onSave(){
+    debugger;
+    const oldData = localStorage.getItem("EmpData");
+    if(oldData != null){
+      const parseData = JSON.parse(oldData);
+      this.employeeForm.controls['empid'].setValue(parseData.length + 1);
+      this.employeeList.unshift(this.employeeForm.value);
+    }else{
+      this.employeeList.unshift(this.employeeForm.value);
+    }
+    localStorage.setItem('EmpData',JSON.stringify(this.employeeList));
+  }
+
 }
---
+++ src/app/model/Employee.ts
@@ -0,0 +1,21 @@
+export class EmployeeModel{
+    empId: number;
+    name: string;
+    city: string;
+    state: string;
+    emailId: string;
+    contactNumber: string;
+    address: string;
+    pincode: string;
+
+    constructor(){
+        this.address = '',
+        this.city = '',
+        this.contactNumber = '',
+        this.state = '',
+        this.emailId = '',
+        this.name = '',
+        this.empId = 0
+        this.pincode = ''
+    }
+}
